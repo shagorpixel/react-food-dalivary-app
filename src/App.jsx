af93b2dad4feb90e7ea40ecd9fdf270f0ac7c4a4
yourusername <shagorpixel@gmail.com>
2025-04-05 16:40:32 +0600

    Setup Porject Install Tailwind CSS & React-Router

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './Components/Nav';
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+      <Nav></Nav>
+      <Outlet></Outlet>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h2>Wellcoem To Home Page</h2>
+        </div>
+    );
+};
+
+export default Home;
